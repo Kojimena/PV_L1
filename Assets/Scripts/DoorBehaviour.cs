7e063aac201a1865e2436292e19a7e286bed8a4d
Jimena Hernández <her21199@uvg.edu.gt>
2025-07-19 09:36:05 -0600

    update final message

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -1,37 +1,66 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro; 
 
 public class DoorBehaviour : MonoBehaviour
 {
-    
-    [SerializeField] private Material openDoorMaterial; // Material de la puerta abierta
-    [SerializeField] private MeshRenderer doorRenderer; // Renderer de la puerta
+    [SerializeField] private Material openDoorMaterial;
+    [SerializeField] private MeshRenderer doorRenderer;
+    [SerializeField] private GameObject customCursor;
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private GameObject fadePanel;
+    [SerializeField] private TextMeshProUGUI finalMessageText;
 
+    private bool hasEscaped = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        
+        messageText.text = "";
+        fadePanel.SetActive(false);
     }
     
+    private void Update()
+    {
+        if (hasEscaped && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (hasEscaped) return;
+
         if (other.CompareTag("Player") && PlayerInventory.hasKey)
         {
             if (doorRenderer != null && openDoorMaterial != null)
             {
                 doorRenderer.material = openDoorMaterial;
             }
+
+            hasEscaped = true;
+            fadePanel.SetActive(true);
+            customCursor.SetActive(false);
+            finalMessageText.text = "Has logrado escapar... esta vez.\n\nPresiona R para reiniciar el nivel.";
         }
         else if (other.CompareTag("Player"))
         {
-            Debug.Log("You need a key to open this door.");
+            ShowMessage("Necesitas una llave para abrir esta puerta.");
         }
     }
+
+    private void ShowMessage(string text)
+    {
+        messageText.text = text;
+        CancelInvoke(nameof(ClearMessage));
+        Invoke(nameof(ClearMessage), 2f); 
+    }
+
+    private void ClearMessage()
+    {
+        messageText.text = "";
+    }
+    
 }
